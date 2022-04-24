0fef86c1ef01f2a96b12fb38df5996b9c38560ec
coderbase-it <48690897+coderbase-it@users.noreply.github.com>
2022-04-25 00:41:01 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 https://github.com/coderbase-it/formation-angular-avance-starter-main
+https://angular.io/guide/service-worker-getting-started
+
+# Branche PWA 
+ng add @angular/pwa
+
+ng serve does not work with service workers, you must use a separate HTTP server to test your project locally
+"pwa-dev":"http-server -p 8080 -c-1 dist/crm" ( package.json)
+http://localhost:8080
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, CoreModule, AppRoutingModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [{ provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' }],
   bootstrap: [AppComponent],
 })
